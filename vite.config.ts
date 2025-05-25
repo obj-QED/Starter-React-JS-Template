23fe37817efe5c4b9f4eeea3a9a0859643fe7c5d
s.ponomariov <obj.qed@gmail.com>
2025-05-25 12:15:53 +0300

    update sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/assets/scss/theme/index" as *;`,
+        sassOptions: {
+          outputStyle: 'compressed',
+        },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
@@ -35,7 +37,7 @@ export default defineConfig({
     },
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: path.resolve(__dirname, 'index.html'),
       },
       output: {
         manualChunks: {
